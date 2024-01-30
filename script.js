8a744529c61369c04938c718a0af5780ccc2938b
mytz <98504002+mytz@users.noreply.github.com>
2024-01-30 18:16:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -24,3 +24,28 @@ chuckyImage.addEventListener('mouseout', function () {
     // Volver a la fuente original de la imagen
     chuckyImage.src = originalChuckySrc;
 });
+
+// Obtener la referencia a la imagen de LUZ
+const luzImage = document.querySelector('.luz');
+
+// Crear un objeto de audio para el sonido de parpadeo
+const parpadeoSound = new Audio('parpadeo.wav');
+
+// Función para cambiar la visibilidad de la imagen de LUZ de manera aleatoria y rápida
+function parpadeoRapido() {
+    // Reproducir el sonido de parpadeo
+    parpadeoSound.play();
+
+    // Generar un número aleatorio entre 0 y 1
+    const aleatorio = Math.random();
+
+    // Establecer la visibilidad según el número aleatorio
+    luzImage.style.opacity = aleatorio > 0.5 ? 1 : 0;
+
+    // Configurar el próximo parpadeo después de un intervalo aleatorio corto
+    const intervaloAleatorio = Math.floor(Math.random() * (200 - 100 + 1) + 100); // Intervalo entre 100 y 200 milisegundos
+    setTimeout(parpadeoRapido, intervaloAleatorio);
+}
+
+// Iniciar el parpadeo rápido
+parpadeoRapido();
